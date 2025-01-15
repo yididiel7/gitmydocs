bfe951856bbcc6d372ea27193afbfd1cdc92f7d9
Tyrone Hills <mobw4u@gmail.com>
2025-01-15 19:28:24 +0200

    created autoinstall script

---
+++ autoappinstall.sh
@@ -0,0 +1,61 @@
+#!/usr/bin/env bash
+
+
+
+
+sudo pacman -S \
+	ansiweather \
+	bat \
+	ccat \
+	celluloid \
+	curl \
+	dict-devils \
+	dict-foldoc \
+	dict-gcide \
+	dict-jargon \
+	dict-moby-thesaurus \
+	dict-wikt-en-all \
+	dict-wn \
+	ffmpeg \
+	gnome-disk-utility \
+	informant \
+	libqalculate \
+	libreoffice-fresh \
+	logo-ls \
+	metar \
+	mpc \
+	mpd \
+	mplayer \
+	ncdu \
+	ncmpcpp \
+	neovim \
+	nitrogen \
+	paru \
+	paru-debug \
+	pinfo \
+	print-manager \
+	pwgen \
+	rig \
+	ripgrep \
+	screenkey \
+	sparklines-git \
+	system-config-printer \
+	thefuck \
+	thunar \
+	thunar-archive-plugin \
+	thunar-media-tags-plugin \
+	thunar-volman \
+	timeshift \
+	tlrc-git \
+	ueberzug \
+	ufw-extras \
+	vim \
+	wcm \
+	weather \
+	wf-shell \
+	wget \
+	wlr-randr \
+	wmctrl \
+	xcowsay \
+	zsh \
+	zsh-syntax-highlighting
